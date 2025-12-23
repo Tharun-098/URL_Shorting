14da673545712ce4ed703dbfa5af165c6be042bc
Tharun-098 <tharun211388@gmail.com>
2025-12-23 16:23:14 +0530

    fix: add production origin to allowedOrigins for CORS

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import linkRouter from './router/linkRoutes.js';
 import cors from "cors"
 const app=express();
-const allowedOrigins=["http://localhost:5174"];
+const allowedOrigins=["http://localhost:5174","https://url-shorting-jdip.vercel.app"];
 app.use(cors({
     origin:allowedOrigins,
     credentials:true,
